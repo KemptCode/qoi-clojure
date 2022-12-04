c8ea2b165e98e465f6e42659d2008b50f32019e9
KemptCode <bar19026@byui.edu>
2022-12-03 20:41:03 -0700

    Starter outline

---
+++ src/decode.clj
@@ -0,0 +1,5 @@
+(defn decode-qoi [bytes]
+    ;; return raw bytes for image
+
+    ;; pixels, width, height, channels, colorspace
+    )
---
+++ src/encode.clj
@@ -0,0 +1,16 @@
+(defn encode-qoi [bytes width height channels]
+    (let [  prevColor   (list 0 0 256)
+            run-length  0           
+            lut         (hash-map)  ;; look up table of seen values
+            ] 
+
+        ;; returns bytes
+        ))
+
+
+(defn do-encode [bytes width height channels]
+    ;; recursive helper function
+
+    ;; returns bytes
+    )
+
